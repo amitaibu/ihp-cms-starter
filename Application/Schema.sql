83f65712bbfbeacfd871efc8b63b661b3923f2f6
Amitai Burstein <amitai@gizra.com>
2023-05-10 14:16:17 +0300

    Add Landing page and paragraphs

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,27 @@
+CREATE FUNCTION set_updated_at_to_now() RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ language plpgsql;
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE landing_pages (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
+CREATE INDEX landing_pages_created_at_index ON landing_pages (created_at);
+CREATE TRIGGER update_landing_pages_updated_at BEFORE UPDATE ON landing_pages FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
+CREATE TABLE paragraph__quotes (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+);
+CREATE TABLE paragraph__ctas (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+);
+CREATE TABLE paragraph__featured_articles (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+);
+CREATE TABLE articles (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    Title TEXT NOT NULL
+);
