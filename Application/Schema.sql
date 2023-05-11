d68b668ca09fed90204b9ce45760116444d79671
Amitai Burstein <amitai@gizra.com>
2023-05-11 15:49:16 +0300

    Fix table names to have a single underscore

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -12,17 +12,17 @@ CREATE TABLE landing_pages (
 );
 CREATE INDEX landing_pages_created_at_index ON landing_pages (created_at);
 CREATE TRIGGER update_landing_pages_updated_at BEFORE UPDATE ON landing_pages FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
-CREATE TABLE paragraph__quotes (
+CREATE TABLE paragraph_quotes (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     landing_page_id UUID NOT NULL,
     weight INT DEFAULT 0 NOT NULL
 );
-CREATE TABLE paragraph__ctas (
+CREATE TABLE paragraph_ctas (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     landing_page_id UUID NOT NULL,
     weight INT DEFAULT 0 NOT NULL
 );
-CREATE TABLE paragraph__featured_articles (
+CREATE TABLE paragraph_featured_articles (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     landing_page_id UUID NOT NULL,
     weight INT DEFAULT 0 NOT NULL
@@ -31,9 +31,9 @@ CREATE TABLE articles (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     Title TEXT NOT NULL
 );
-CREATE INDEX paragraph__quotes_landing_page_id_index ON paragraph__quotes (landing_page_id);
-CREATE INDEX paragraph__ctas_landing_page_id_index ON paragraph__ctas (landing_page_id);
-CREATE INDEX paragraph__featured_articles_landing_page_id_index ON paragraph__featured_articles (landing_page_id);
-ALTER TABLE paragraph__ctas ADD CONSTRAINT paragraph__ctas_ref_landing_page_id FOREIGN KEY (landing_page_id) REFERENCES landing_pages (id) ON DELETE NO ACTION;
-ALTER TABLE paragraph__featured_articles ADD CONSTRAINT paragraph__featured_articles_ref_landing_page_id FOREIGN KEY (landing_page_id) REFERENCES landing_pages (id) ON DELETE NO ACTION;
-ALTER TABLE paragraph__quotes ADD CONSTRAINT paragraph__quotes_ref_landing_page_id FOREIGN KEY (landing_page_id) REFERENCES landing_pages (id) ON DELETE NO ACTION;
+CREATE INDEX paragraph_quotes_landing_page_id_index ON paragraph_quotes (landing_page_id);
+CREATE INDEX paragraph_ctas_landing_page_id_index ON paragraph_ctas (landing_page_id);
+CREATE INDEX paragraph_featured_articles_landing_page_id_index ON paragraph_featured_articles (landing_page_id);
+ALTER TABLE paragraph_ctas ADD CONSTRAINT paragraph_ctas_ref_landing_page_id FOREIGN KEY (landing_page_id) REFERENCES landing_pages (id) ON DELETE NO ACTION;
+ALTER TABLE paragraph_featured_articles ADD CONSTRAINT paragraph_featured_articles_ref_landing_page_id FOREIGN KEY (landing_page_id) REFERENCES landing_pages (id) ON DELETE NO ACTION;
+ALTER TABLE paragraph_quotes ADD CONSTRAINT paragraph_quotes_ref_landing_page_id FOREIGN KEY (landing_page_id) REFERENCES landing_pages (id) ON DELETE NO ACTION;
