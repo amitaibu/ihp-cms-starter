300d6cdb4f73785cf53753e73538574c6483072d
Amitai Burstein <amitai@gizra.com>
2023-05-11 18:00:30 +0300

    Change homepge

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -21,7 +21,7 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
     <title>{pageTitleOrDefault "App"}</title>
 </head>
 <body>
-    <div class="container-wide flex flex-col gap-y-4">
+    <div class="container-wide flex flex-col gap-y-4 mt-10">
         {renderFlashMessages}
         {inner}
     </div>
