2f8d9dd534d0b9bc0eee35c579fac71abfd841b2
Amitai Burstein <amitai@gizra.com>
2023-05-11 22:04:41 +0300

    trigger error

--- Web/Controller/LandingPages.hs
+++ Web/Controller/LandingPages.hs
@@ -37,6 +37,7 @@ instance Controller LandingPagesController where
         where
             -- We will render each paragraph with it's own renderer, and have a tuple
             -- with the weight, and the `hsx` result. Then we can sort the list by weight.
+            -- @todo: Why do we need to type hint the paragraph?
             paragraphCtas' = paragraphCtas
                     |> fmap (\(paragraph :: ParagraphCta) -> (paragraph.weight, ParagraphCtas.renderParagraph paragraph.title))
 
@@ -46,10 +47,9 @@ instance Controller LandingPagesController where
             -- @todo: Is there a better way, so render happens inside the Show instead of here.
             -- Currently without rendering, we can't place all in a single List, as they have different
             -- types.
-
-            -- allParagraphsRendered = paragraphCtas' ++ paragraphQuotes'
+            allParagraphsRendered = merge paragraphCtas' paragraphQuotes'
                 -- Sort the paragraphs by weight.
-                -- |> sortOn fst
+                -- |> sort
                 -- Get the `hsx` result.
                 -- |> fmap snd
 
@@ -89,5 +89,8 @@ instance Controller LandingPagesController where
 buildLandingPage landingPage = landingPage
     |> fill @'["title", "slug"]
 
-merge [] ys = ys
-merge (x:xs) ys = x:merge ys xs
+-- https://stackoverflow.com/a/70335119/750039
+merge :: [a] -> [a] -> [a]
+merge xs     []     = xs
+merge []     ys     = ys
+merge (x:xs) (y:ys) = x : y : merge xs ys
