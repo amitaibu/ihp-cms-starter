47d41c14e088ae45e735c386d964c1e28da9d1dc
Amitai Burstein <amitai@gizra.com>
2023-05-11 16:05:10 +0300

    Add TW container

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -21,7 +21,7 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
     <title>{pageTitleOrDefault "App"}</title>
 </head>
 <body>
-    <div class="container mt-4">
+    <div class="container-wide flex flex-col gap-y-4">
         {renderFlashMessages}
         {inner}
     </div>
@@ -43,6 +43,8 @@ scripts = [hsx|
         {when isDevelopment devScripts}
         <script src={assetPath "/vendor/jquery-3.6.0.slim.min.js"}></script>
         <script src={assetPath "/vendor/timeago.js"}></script>
+        <script src={assetPath "/vendor/popper.min.js"}></script>
+        <script src={assetPath "/vendor/bootstrap-5.2.1/bootstrap.min.js"}></script>
         <script src={assetPath "/vendor/flatpickr.js"}></script>
         <script src={assetPath "/vendor/morphdom-umd.min.js"}></script>
         <script src={assetPath "/vendor/turbolinks.js"}></script>
