1ef84638a2205d62513e54b0791d06480b65e309
Amitai Burstein <amitai@gizra.com>
2023-05-13 20:58:15 +0300

    Show sortable handle only when we have multiple Paragraphs

--- Web/View/LandingPages/Edit.hs
+++ Web/View/LandingPages/Edit.hs
@@ -66,11 +66,7 @@ orderAndRenderParagraphs ctas quotes =
             [hsx|
                 <li class="flex flex-row gap-2 items-baseline">
 
-                    <div class="sortable-handle">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-200 hover:text-gray-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                        </svg>
-                    </div>
+                    {sortableHandle}
 
                     <input type="hidden" name="paragraphId" value={show id} />
 
@@ -80,3 +76,19 @@ orderAndRenderParagraphs ctas quotes =
                     </div>
                 </li>
             |]
+
+        sortableHandle =
+            if
+                length allSorted > 1
+            then
+                [hsx|
+                    <div class="sortable-handle">
+                        <div class="sortable-handle">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-200 hover:text-gray-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
+                        </div>
+                    </div>
+                |]
+            else
+                mempty
