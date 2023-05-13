663ee883e3427c0f5ecf8171870616d7954f0c3b
Amitai Burstein <amitai@gizra.com>
2023-05-13 17:30:56 +0300

    Switch to IHP master

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
 let
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/ihp.git";
-        ref = "refs/tags/v1.0.1";
+        # ref = "refs/tags/v1.0.1";
+        # If changing to a specific `rev` Execute:
+        # nix-shell --run 'make build/ihp-lib'
+        rev = "b24b7fda2f7c7d66fc055613bd7677e05427ec21";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
