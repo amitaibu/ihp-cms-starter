6c5f1e766f53732fec2f8ae770e8cc30c61a39fe
Amitai Burstein <amitai@gizra.com>
2023-05-13 21:57:51 +0200

    Show sortable handle only when we have multiple Paragraphs (#5)

--- Web/View/LandingPages/Edit.hs
+++ Web/View/LandingPages/Edit.hs
@@ -1,6 +1,6 @@
 module Web.View.LandingPages.Edit where
 import Web.View.Prelude
-
+import Web.Controller.Prelude
 
 data EditView = EditView { landingPage :: Include' ["paragraphCtas", "paragraphQuotes"] LandingPage }
 
@@ -44,39 +44,53 @@ renderForm landingPage = formFor landingPage [hsx|
 
         <div>{submitButton}</div>
     </div>
-
 |]
 
--- orderAndRenderParagraphs :: (?context::ControllerContext) => [ParagraphCta] -> [ParagraphQuote] -> Text.Blaze.Html.Html
+orderAndRenderParagraphs :: [ParagraphCta] -> [ParagraphQuote] -> Html
 orderAndRenderParagraphs ctas quotes =
     [hsx|{forEach allSorted (\rendered -> rendered)}|]
     where
         ctas' = ctas
-            |> fmap (\paragraph -> (paragraph.weight, paragraphTitleAndOps EditParagraphCtaAction paragraph.id paragraph.title ("CTA" :: Text)))
+            |> fmap (\paragraph -> (paragraph.weight, paragraphTitleAndOps EditParagraphCtaAction DeleteParagraphCtaAction paragraph.id paragraph.title ("CTA" :: Text)))
 
         quotes' = quotes
-            |> fmap (\paragraph -> (paragraph.weight, paragraphTitleAndOps EditParagraphQuoteAction paragraph.id paragraph.title ("Quote" :: Text)))
+            |> fmap (\paragraph -> (paragraph.weight, paragraphTitleAndOps EditParagraphQuoteAction DeleteParagraphQuoteAction paragraph.id paragraph.title ("Quote" :: Text)))
+
 
         allSorted = ctas' ++ quotes'
             |> sortOn fst
             |> fmap snd
 
-        paragraphTitleAndOps action id title type_  =
-            -- Show the paragraph title and the operations to perform on it.
+        -- Show the paragraph title and the operations to perform on it.
+        paragraphTitleAndOps :: (Show (PrimaryKey record), AutoRoute controller, HasPath controller) => (Id' record -> controller) -> (Id' record -> controller) -> Id' record -> Text -> Text -> Html
+        paragraphTitleAndOps editAction deleteAction id title type_  =
             [hsx|
                 <li class="flex flex-row gap-2 items-baseline">
 
-                    <div class="sortable-handle">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-200 hover:text-gray-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                        </svg>
-                    </div>
+                    {sortableHandle}
 
                     <input type="hidden" name="paragraphId" value={show id} />
 
                     {title} <span class="text-sm text-gray-600">({type_})</span>
-                    <div>
-                        <a href={pathTo $ action id} class="text-blue-500 text-sm hover:underline">Edit</a>
+                    <div class="flex flex-row gap-2">
+                        <a href={pathTo $ editAction id} class="text-blue-500 text-sm hover:underline">Edit</a>
+                        <a href={pathTo $ deleteAction id} class="js-delete text-blue-500 text-sm hover:underline">Delete</a>
                     </div>
                 </li>
             |]
+
+        sortableHandle =
+            if
+                length ctas + length quotes > 1
+            then
+                [hsx|
+                    <div class="sortable-handle">
+                        <div class="sortable-handle">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-200 hover:text-gray-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
+                        </div>
+                    </div>
+                |]
+            else
+                mempty
