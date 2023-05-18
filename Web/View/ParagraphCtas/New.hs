b06cdb6e4f8b3ccc2d02d8966a5a1364ab6f3675
Amitai Burstein <amitai@gizra.com>
2023-05-18 22:51:33 +0200

    Add reference to Landing page from CTA (#9)
    * Add REf to LP from CTA
    
    * Add select list
    
    * Improve labels

--- Web/View/ParagraphCtas/New.hs
+++ Web/View/ParagraphCtas/New.hs
@@ -1,13 +1,18 @@
 module Web.View.ParagraphCtas.New where
 import Web.View.Prelude
+import Web.Element.Types
+import Web.Element.ElementWrap
 
-data NewView = NewView { paragraphCta :: ParagraphCta }
+data NewView = NewView
+    { paragraphCta :: ParagraphCta
+    , landingPages :: [LandingPage]
+    }
 
 instance View NewView where
     html NewView { .. } = [hsx|
         {breadcrumb}
         <h1>New ParagraphCta</h1>
-        {renderForm paragraphCta}
+        {renderForm paragraphCta landingPages}
     |]
         where
             breadcrumb = renderBreadcrumb
@@ -15,12 +20,25 @@ instance View NewView where
                 , breadcrumbText "New ParagraphCta"
                 ]
 
-renderForm :: ParagraphCta -> Html
-renderForm paragraphCta = formFor paragraphCta [hsx|
+renderForm :: ParagraphCta -> [LandingPage] -> Html
+renderForm paragraphCta landingPages = formFor paragraphCta [hsx|
     {(hiddenField #landingPageId)}
     {(hiddenField #weight)}
-    {(textField #title) {required = True}}
-    {(textareaField #body) {required = True}}
-    {submitButton}
+    {visibleForm paragraphCta landingPages}
 
 |]
+    where
+            visibleForm paragraphCta landingPages =
+                [hsx|
+                    {(textField #title) {required = True}}
+                    {(textareaField #body) {required = True}}
+                     {(selectField #refLandingPageId landingPages) {required = True, fieldLabel = "Landing page", helpText = "Select the landing page you want to link to."}}
+                    {submitButton}
+                |]
+                |> wrapContainerVerticalSpacing AlignNone
+                |> wrapContainerWide
+
+instance CanSelect LandingPage where
+    type SelectValue LandingPage = Id LandingPage
+    selectValue landingPage = landingPage.id
+    selectLabel landingPage = landingPage.title
--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
         # ref = "refs/tags/v1.0.1";
         # If changing to a specific `rev` Execute:
         # nix-shell -j auto --cores 0 --run 'rm -rf build/ihp-lib; make -B build/ihp-lib; rm .envrc; make -B .envrc; direnv allow; make -B build/Generated/Types.hs;'
-        rev = "b24b7fda2f7c7d66fc055613bd7677e05427ec21";
+        rev = "4fd83ca74fe80776bb188ec7f34f18770f5d1d69";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
