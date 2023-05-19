43318f2810d789b5421e2deb01333d76decbe20f
Amitai Burstein <amitai@gizra.com>
2023-05-19 12:03:53 +0300

    Move generic ctrl code around

--- Application/Helper/Controller.hs
+++ Application/Helper/Controller.hs
@@ -1,5 +1,22 @@
 module Application.Helper.Controller where
 
 import IHP.ControllerPrelude
+import Generated.Types
 
 -- Here you can add functions which are available in all your controllers
+
+fetchLandingPageWithParagraphs :: (?modelContext :: ModelContext) => Id LandingPage -> IO (Include' ["paragraphCtasLandingPages", "paragraphQuotes"] LandingPage)
+fetchLandingPageWithParagraphs landingPageId = do
+    fetch landingPageId
+        >>= pure . modify #paragraphCtasLandingPages (orderByDesc #weight)
+        >>= pure . modify #paragraphQuotes (orderByDesc #weight)
+        >>= fetchRelated #paragraphCtasLandingPages
+        >>= fetchRelated #paragraphQuotes
+
+
+getParagraphsCount :: (?modelContext::ModelContext) => Id LandingPage -> IO Int
+getParagraphsCount landingPageId = do
+    landingPage <- fetchLandingPageWithParagraphs landingPageId
+    pure $ length landingPage.paragraphCtasLandingPages
+                    + length landingPage.paragraphQuotes
+                    + 1
