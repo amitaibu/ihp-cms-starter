49ae5023d4bf09f09a39a58aaf91d07dcb3eea7f
Amitai Burstein <amitai@gizra.com>
2023-05-21 11:03:22 +0300

    Require file upload

--- Web/View/ParagraphQuotes/Form.hs
+++ Web/View/ParagraphQuotes/Form.hs
@@ -18,14 +18,7 @@ renderForm paragraphQuote isImageRequired = formFor paragraphQuote [hsx|
                     {(textareaField #body) {required = True}}
                     {(textField #subtitle) {required = True}}
 
-                    <input
-                        type="file"
-                        name="imageUrl"
-                        class="form-control-file"
-                        accept="image/*"
-                        data-preview="#imageUrlPreview"
-                        required={isImageRequired}
-                    />
+                    {(fileField #imageUrl) {required = True, additionalAttributes = [("accept", "image/*"), ("data-preview", "#imageUrlPreview")]}}
 
                     <img id="imageUrlPreview" src={paragraphQuote.imageUrl} class={imageClasses} />
 
