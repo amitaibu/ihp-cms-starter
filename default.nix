d96fab43499701ff4766a50007ee82698a942943
Amitai Burstein <amitai@gizra.com>
2023-05-30 21:36:00 +0300

    Update nix

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
         # ref = "refs/tags/v1.0.1";
         # If changing to a specific `rev` Execute:
         # nix-shell -j auto --cores 0 --run 'rm -rf build/ihp-lib; make -B build/ihp-lib; rm .envrc; make -B .envrc; direnv allow; make -B build/Generated/Types.hs;'
-        rev = "ab0611ee456fc1b9896d51881976663f0ae1292d";
+        rev = "99084417ac3a05d27bec0dcf2b911f6464e6571c";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
