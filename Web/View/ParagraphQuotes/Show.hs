ac3aa6b930d21d4695cc1e345eeec9567fc4f6b1
Amitai Burstein <amitai@gizra.com>
2023-05-31 18:13:41 +0300

    Fix pattern match

--- Web/View/ParagraphQuotes/Show.hs
+++ Web/View/ParagraphQuotes/Show.hs
@@ -34,7 +34,9 @@ renderParagraph body subtitle imageUrl =
 
         -- Sign the image URL to prevent tampering.
         Config.PublicAndPrivateKeys (_, privateKey) = getAppConfig @Config.PublicAndPrivateKeys
-        (Right signed) = RSA.sign Nothing (Just Hash.Algorithms.MD5) privateKey (cs $ imageUrl <> "400x200")
+        signed = case RSA.sign Nothing (Just Hash.Algorithms.MD5) privateKey (cs $ imageUrl <> "400x200") of
+            Left _ -> ""
+            Right signature -> signature
 
         -- https://iconmonstr.com/quote-3-svg/
         quotationSign = [hsx|
