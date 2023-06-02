0bc0d77c48c5656f79226363cfa727ab309294e9
Amitai Burstein <amitai@gizra.com>
2023-06-02 11:36:42 +0300

    Try to read keys

--- Config/Config.hs
+++ Config/Config.hs
@@ -3,9 +3,12 @@ module Config where
 import IHP.Prelude
 import IHP.Environment
 import IHP.FileStorage.Config
-import IHP.FrameworkConfig
+import IHP.FrameworkConfig ( ConfigBuilder, option )
 import Web.View.CustomCSSFramework
 import Crypto.PubKey.RSA as RSA
+import Control.Exception (catch)
+import qualified Data.ByteString as BS
+import Web.JWT
 
 data RsaPublicAndPrivateKeys = RsaPublicAndPrivateKeys { publicKey :: RSA.PublicKey, privateKey :: RSA.PrivateKey }
 
@@ -15,9 +18,25 @@ config = do
     -- for what you can do here
     option customTailwind
 
+    -- Static directory.
+    initStaticDirStorage
+
     -- Private and public keys to sign and verify image style URLs.
     (publicKey, privateKey) <- liftIO $ liftIO $ RSA.generate 300 65537
     option $ RsaPublicAndPrivateKeys publicKey privateKey
 
-    -- Static directory.
-    initStaticDirStorage
+        -- Private and public keys to sign and verify image style URLs.
+    privateKeyContent <- liftIO $ readRsaKeyFromFile "./Config/id_rsa"
+    publicKeyContent <- liftIO $ readRsaKeyFromFile "./Config/id_rsa.pub"
+
+    case (readRsaSecret privateKeyContent, readRsaPublicKey publicKeyContent) of
+        (Just privateKey, Just publicKey) -> option $ RsaPublicAndPrivateKeys publicKey privateKey
+        _ -> error "Failed to read RSA keys, please execute from the root of your project: ssh-keygen -t rsa -f ./Config/id_rsa"
+
+
+readRsaKeyFromFile :: FilePath -> IO BS.ByteString
+readRsaKeyFromFile path = do
+    catch (BS.readFile path) handleException
+  where
+    handleException :: IOError -> IO BS.ByteString
+    handleException _ = return BS.empty
--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ let
             text
             hlint
             p.ihp
-            cryptonite
+            jwt
         ];
         otherDeps = p: with p; [
             # Native dependencies, e.g. imagemagick
