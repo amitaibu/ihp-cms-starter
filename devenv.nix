0ef8a4d7edeac793e038197e1c2403b2b8882991
Amitai Burstein <amitai@gizra.com>
2023-06-03 16:36:51 +0300

    indentation

--- devenv.nix
+++ devenv.nix
@@ -1,31 +1,30 @@
-    { pkgs, inputs, config, ... }:
+{ pkgs, inputs, config, ... }:
 
-    {
-        # See full reference at https://devenv.sh/reference/options/
-        # For IHP specific options, see https://ihp.digitallyinduced.com/Guide/package-management.html
+{
+    # See full reference at https://devenv.sh/reference/options/
+    # For IHP specific options, see https://ihp.digitallyinduced.com/Guide/package-management.html
 
-        imports = [
-            "${inputs.ihp}/NixSupport/devenv.nix"
-        ];
+    imports = [
+        "${inputs.ihp}/NixSupport/devenv.nix"
+    ];
 
-        # https://devenv.sh/packages/
-        packages = with pkgs; [
-            # Native dependencies, e.g. imagemagick
-            imagemagick
-            nodejs
-        ];
+    # https://devenv.sh/packages/
+    packages = with pkgs; [
+        # Native dependencies, e.g. imagemagick
+        imagemagick
+        nodejs
+    ];
 
-        ihp.enable = true;
-        ihp.projectPath = ./.;
+    ihp.enable = true;
+    ihp.projectPath = ./.;
 
-        ihp.haskellPackages = p: with p; [
-            # Haskell dependencies go here
-            cabal-install
-            base
-            wai
-            text
-            hlint
-            ihp
-
-        ];
-    }
+    ihp.haskellPackages = p: with p; [
+        # Haskell dependencies go here
+        cabal-install
+        base
+        wai
+        text
+        hlint
+        ihp
+    ];
+}
