53cb766e188443a78399ebe8568bb991be4ed260
Amitai Burstein <amitai@gizra.com>
2023-06-24 00:08:44 +0300

    Fix test

--- Test/Main.hs
+++ Test/Main.hs
@@ -2,6 +2,19 @@ module Main where
 
 import Test.Hspec
 import IHP.Prelude
+import IHP.FrameworkConfig
+import IHP.Test.Mocking
+import IHP.HaskellSupport
+import IHP.ModelSupport
+import Config
+
+import Generated.Types
+import Web.Routes
+import Web.Types
+import Web.FrontController
+import Network.Wai
+import IHP.ControllerPrelude
+import IHP.ViewPrelude
 
 main :: IO ()
 main = hspec do
