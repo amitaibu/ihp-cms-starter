420ce158c7f1423a2f8b6f43f823a8e60c43296b
Amitai Burstein <amitai@gizra.com>
2023-08-14 23:44:53 +0300

    Add GH action for testing (#20)

---
+++ Test/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Test.Hspec
+import IHP.Prelude
+import IHP.FrameworkConfig
+import IHP.Test.Mocking
+import IHP.HaskellSupport
+import IHP.ModelSupport
+import Config
+
+import Generated.Types
+import Web.Routes
+import Web.Types
+import Web.FrontController
+import Network.Wai
+import IHP.ControllerPrelude
+import IHP.ViewPrelude
+
+main :: IO ()
+main = hspec do
+    tests
+
+tests :: Spec
+tests = aroundAll (withIHPApp WebApplication config) do
+    describe "Test" do
+        it "should work" $ withContext do
+            True `shouldBe` True
