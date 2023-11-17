432e54392b2f4ecc383caec2fbffaa79556651f3
Amitai Burstein <amitai@gizra.com>
2023-11-17 17:19:45 +0200

    Fix serialize

--- Web/Controller/Projects.hs
+++ Web/Controller/Projects.hs
@@ -7,16 +7,16 @@ import Database.PostgreSQL.Simple.ToField
 import Data.ByteString.Builder (byteString, char8)
 
 
-instance ToField (ProjectType, Text) where
+instance ToField (ProjectType, Int) where
     toField = serializeTuple
 
 
-serializeTuple :: (ProjectType, Text) -> Action
+serializeTuple :: (ProjectType, Int) -> Action
 serializeTuple (projectType, participants) = Many
     [ Plain (byteString "(")
     , toField projectType
     , Plain (char8 ',')
-    , toField participants
+    , toField $ show participants
     , Plain (char8 ')')
     ]
 
@@ -24,10 +24,9 @@ instance Controller ProjectsController where
     action ProjectsAction = do
         projects <- query @Project |> fetch
 
-        -- Fetch only specific projects by a pair of values.
-        -- In this case we really fetch all, but show how we pair the values we want to
-        -- query by.
-        let pairs = projects |> fmap (\project -> (project.projectType, project.participants))
+
+        -- let pairs = projects |> fmap (\project -> (project.projectType, project.participants))
+        let pairs = [(ProjectTypeOngoing, 1 :: Int), (ProjectTypeFinished, 2)]
 
         -- Use a parameterized query to handle list of tuples
         projectsQuery :: [Project] <- sqlQuery "SELECT * FROM projects WHERE (project_type, participants) IN ?" (Only $ In pairs)
