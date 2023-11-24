100bbf30eed6ce3ef41bcc9591240046ea50ed02
Aron Novak <aron@gizra.com>
2023-11-24 10:06:10 +0100

    deployment attempt

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         systems.follows = "ihp/systems";
     };
 
-    outputs = inputs@{ ihp, flake-parts, systems, ... }:
+    outputs = inputs@{ self, nixpkgs, ihp, flake-parts, systems, ... }:
         flake-parts.lib.mkFlake { inherit inputs; } {
 
             systems = import systems;
@@ -34,6 +34,32 @@
                     ];
                 };
             };
+
+            # Adding the new NixOS configuration for "ihp-app"
+            flake.nixosConfigurations."ihp-app" = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = inputs;
+                modules = [
+                    "${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
+                    ihp.nixosModules.appWithPostgres
+                    ({ ... }: {
+                        security.acme.defaults.email = "no-reply@tpp-qa.gizra.site";
+
+                        services.ihp = {
+                            domain = "tpp-qa.gizra.site";
+                            migrations = ./Application/Migration;
+                            schema = ./Application/Schema.sql;
+                            fixtures = ./Application/Fixtures.sql;
+                            sessionSecret = "M$cmzMCEx7xfL-5_q6%9cpve_0BAd5BbDaOtzhv7";
+                        };
+
+                        swapDevices = [ { device = "/swapfile"; size = 8192; } ];
+
+                        system.stateVersion = "23.05";
+                    })
+                ];
+            };
+
 
         };
 }
