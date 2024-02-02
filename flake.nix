21dad89db3ba40ecfc531b7e4fd04432d04fb427
Aron Novak <aron@gizra.com>
2024-02-02 14:07:31 +0100

    Revert "test"
    This reverts commit b157697b0cbae8658d638b06888b63362540e533.

--- flake.nix
+++ flake.nix
@@ -51,8 +51,8 @@
 
                         security.acme.defaults.email = "no-reply@tpp-qa.gizra.site";
                         security.acme.acceptTerms = true;
-                        systemd.services.nginx.virtualHosts."tpp-qa.gizra.site".enableACME = false;
-                        systemd.services.nginx.virtualHosts."tpp-qa.gizra.site".forceSSL = false;
+                        services.nginx.virtualHosts."tpp-qa.gizra.site".enableACME = false;
+                        services.nginx.virtualHosts."tpp-qa.gizra.site".forceSSL = false;
 
                         services.ihp = {
                             domain = "tpp-qa.gizra.site";
