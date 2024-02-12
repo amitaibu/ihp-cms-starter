b67d1cef4712e589b3ce0a1dfac3cb390fce1b07
Aron Novak <aron@gizra.com>
2024-02-12 13:14:11 +0100

    Revert "switch to fork"
    This reverts commit 354f96230b1c5c74788b4607b123691fa95cf39d.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        ihp.url = "github:aronnovak/ihp/deploy-to-nixos-fixes";
+        ihp.url = "github:digitallyinduced/ihp/deploy-to-nixos-fixes";
         nixpkgs.follows = "ihp/nixpkgs";
         flake-parts.follows = "ihp/flake-parts";
         devenv.follows = "ihp/devenv";
