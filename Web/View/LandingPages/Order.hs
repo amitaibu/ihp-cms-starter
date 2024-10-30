f9422fd0ae630070de753d1063727a336fc413f6
Amitai Burstein <amitai@gizra.com>
2024-10-30 17:43:46 +0000

    Move re-order paragraphs to own page

---
+++ Web/View/LandingPages/Order.hs
@@ -0,0 +1,64 @@
+module Web.View.LandingPages.Order where
+
+import Web.Element.Cta
+import Web.Element.ElementWrap
+import Web.Element.Link
+import Web.Element.Types
+import Web.Element.Quote
+import Web.Types
+import Web.View.Prelude
+
+data OrderParagraphsView = OrderParagraphsView { landingPageWithRecords :: LandingPageWithRecords }
+
+instance View OrderParagraphsView where
+    html OrderParagraphsView { .. } = [hsx|
+        {header}
+
+        {orderAndRenderParagraphs landingPageWithRecords }
+    |]
+        where
+            landingPage = landingPageWithRecords.landingPage
+
+            breadcrumb = renderBreadcrumb
+                            [ breadcrumbLink "LandingPages" LandingPagesAction
+                            , breadcrumbText "Show LandingPage"
+                            ]
+
+            header = [hsx|
+                    {breadcrumb}
+                    {titleAndEdit}
+                |]
+                    |> wrapVerticalSpacing AlignNone
+                    |> wrapContainerWide
+
+
+            titleAndEdit =
+                [ cs landingPage.title |> wrapHeaderTag 1
+                , renderLinkAction (EditLandingPageAction landingPage.id) "Edit"
+                ]
+                    |> mconcat
+                    |> wrapHorizontalSpacingTiny AlignBaseline
+
+
+orderAndRenderParagraphs :: (?context::ControllerContext) => LandingPageWithRecords -> Html
+orderAndRenderParagraphs landingPageWithRecords =
+    ctas ++ quotes
+        -- Order by weight.
+        |> sortOn fst
+        |> fmap snd
+        |> mconcat
+    where
+
+        ctas = landingPageWithRecords.paragraphCtas
+            |> fmap (\paragraph ->
+                ( paragraph.weight
+                , paragraph.title
+                )
+            )
+
+        quotes = landingPageWithRecords.paragraphQuotes
+            |> fmap (\paragraph ->
+                ( paragraph.weight
+                , paragraph.body
+                )
+            )
